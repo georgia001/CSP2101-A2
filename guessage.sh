a79f705a0aa47d9a4e9a8fce69aeec4a8c06eda7
georgia001 <62577466+georgia001@users.noreply.github.com>
2020-04-09 22:20:05 +1000

    Update guessage.sh

--- guessage.sh
+++ guessage.sh
@@ -1,1 +1,18 @@
+#!/usr/bash/bin
+#Georgia Robson 10477706 CSP2101 A2 T1
 
+echo -e "Guess a number between 1 and 100.\n"
+
+#random number generator
+((answer = RANDOM % 100 + 1))
+
+#Playing the game 
+while ((guess != answer)); do
+  read -p "Enter guess $num: " guess
+  if ((guess < answer )); then 
+    echo "Higher"
+  elif ((guess > answer)); then
+    echo "Lower"
+  fi
+done
+echo -e "You guessed correct.\n"
